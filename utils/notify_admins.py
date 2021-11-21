84af06b68309d8f594367d69a47c3e59918c9dfd
dynamitebilol <92247901+dynamitebilol@users.noreply.github.com>
2021-11-21 11:43:38 +0500

    update

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -6,9 +6,8 @@ from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
+            await dp.bot.send_message(ADMINS, "Bot ishga tushdi")
 
         except Exception as err:
             logging.exception(err)
