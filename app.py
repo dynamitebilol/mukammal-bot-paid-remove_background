2a4cbbfebcf0d141b60bad484b490c50e8e4441e
dynamitebilol <92247901+dynamitebilol@users.noreply.github.com>
2021-11-21 10:51:52 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from aiogram import executor
+from utils.db_api.sqlite import Database
+from loader import dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
+
+
+async def on_startup(dispatcher):
+    # Birlamchi komandalar (/star va /help)
+    await set_default_commands(dispatcher)
+
+    # Ma'lumotlar bazasini yaratamiz:
+    try:
+        db.create_table_users()
+    except Exception as err:
+        print(err)
+
+    # Bot ishga tushgani haqida adminga xabar berish
+    await on_startup_notify(dispatcher)
+
+
+db = Database(path_to_db="data/main.db")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
---
+++ handlers/users/__init__.py
@@ -0,0 +1,5 @@
+from . import help
+from . import start
+from . import admin
+from . import telegraph
+from . import echo
